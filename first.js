4b9b82461d04dabc9e6abd42ee21d761a95ee8c0
muhammadahmed144 <muhammadahmedmohsin123@gmail.com>
2025-06-25 00:46:48 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,50 @@
+//CHAPTER NO 01;
+//QUESTION NO 01;
+alert("Welcome! to my website");
+//QUESTION NO 02;
+alert("Error! Please enter a valid password.")
+//QUESTION NO 03;
+alert("Welcome to Js Land...Happy Coding.")
+//QUESTION NO 04;
+alert("welcome to Js Land...")
+alert("prevent this page from creating additional dialogs.")
+//QUESTION NO 05;
+onsole.log ("HELLO! i can run js through my web browsers console");
+//QUESTION NO 06;
+//QUESTION NO 07;
+
+function showAlert(){
+alert("Hello! You clicked the button.");
+}
+
+//CHAPTER N0 02;
+//QUESTION NO 01;
+var username
+//QUESTION NO 02;
+var myName
+FullName= "Muhammad Ahmed Mohsin"
+//QUESTION NO 03;
+var message
+var message ="Hello World"
+alert("message")
+//QUESTION NO 04;
+alert("Muhammad Ahmed")
+alert("14 years old")
+alert("Smit student modern web & app developer")
+//QUESTION NO 05;
+alert("PIZZA" +"\n" +"PIZZ"+"\n"+"PIZ"+"\n"+"PI"+"\n"+"P")
+//QUESTION NO 06;
+var email
+email ="Email Address"   
+alert("My email address ahmed@gmail.com")
+//QUESTION NO 07;
+var book
+alert("I am trying to Learn from the BookA smarter \n way to Learn JavaScript")
+//QUESTION NO 08;
+document.writeln("Yah I can write HTML content through JavaScript")
+//QUESTION NO 09;
+alert("═════════≼≽❈❉❈≼≽═════════")
+
+
+//END
+
